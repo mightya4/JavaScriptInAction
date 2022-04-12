893dc1d13d53eb3cf77138a238fc35155254fbe8
Akeem <akeem.mighty@gmail.com>
2022-04-12 11:31:00 -0400

    Created Prompts for Animal and Color in Worksheet.js File

--- worksheet.js
+++ worksheet.js
@@ -4,3 +4,11 @@ console.log(dayOfWeek);
 dayOfWeek = "Friday";
 console.log("I can’t wait for "+ dayOfWeek +"!");
 
+
+let animalInput = prompt("What is your favorite animal?");
+let colorInput = prompt("What is your favorite color?");
+
+let animalPhrase = "I've never seen a "+ colorInput + " " + animalInput + "!";
+console.log(animalPhrase);
+
+
